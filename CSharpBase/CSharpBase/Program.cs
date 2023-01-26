4fa0b02bde1f0bf6397cd7efdc00450572575217
Roman <mr.gilev2016@mail.ru>
2023-01-26 19:42:39 +0600

    created a calculator project

---
+++ CSharpBase/CSharpBase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpBase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CSharpBase/Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            int x = 25;
+            int y = 30;
+            int c = x + y;
+            Console.Write("ответ: " + c);
+         }
+    }
+}
